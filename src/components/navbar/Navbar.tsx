d4091883e61ea0be8b040952713864307c50304d
El Ghalbzouri Adnan <elghalbzouriadnan@gmail.com>
2023-11-18 23:06:23 +0100

    mises en place du navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,7 +6,11 @@ import { NavLink } from "react-router-dom";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  const [showMenu, setShowMenu] = useState(true);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  }
 
   return (
     <header>
@@ -26,10 +30,24 @@ const Navbar = () => {
             <li>Communauté</li>
           </NavLink>
         </ul>
-        <button onClick={() => setShowMenu(!showMenu)}>
+        <ul className={showMenu ? "navbar__menu-visible" : "navbar__menu-hidden"}>
+          <NavLink to="/" className={(nav) => (nav.isActive ? "active" : "")}>
+            <li>Accueil</li>
+          </NavLink>
+          <NavLink to="/profile">
+            <li>Profil</li>
+          </NavLink>
+          <NavLink to="/settings">
+            <li>Réglages</li>
+          </NavLink>
+          <NavLink to="/community">
+            <li>Communauté</li>
+          </NavLink>
+        </ul>
+        <button onClick={handleClick}>
           <img
-            src={showMenu ? hamburger : close}
-            alt={showMenu ? "open menu" : "close menu"}
+            src={showMenu ? close : hamburger}
+            alt={showMenu ? "close menu" : "open menu"}
           />
         </button>
       </nav>
