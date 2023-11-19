ee2a201ff26229e85ea73e94bcb70d29245ee6da
El Ghalbzouri Adnan <elghalbzouriadnan@gmail.com>
2023-11-19 18:23:50 +0100

    fin de la navbar et ajout des type sur kpiobjectif

--- src/components/kpiobjective/KpiObjective.tsx
+++ src/components/kpiobjective/KpiObjective.tsx
@@ -13,10 +13,28 @@ type DataUserAverageSessionsType = {
   sessionLength: number;
 }[];
 
+type PayloadType = {
+  chartType: undefined;
+  color: string;
+  dataKey: string;
+  fill: string;
+  formatter: undefined;
+  name: string;
+  opacity: number;
+  payload: { day: number; sessionLength: number };
+  stroke: string;
+  strokeWidth: number;
+  type: undefined;
+  unit: undefined;
+  value: number;
+}[]
+
+type Points = { x: number; y: number }[];
+
 
 const KpiObjective = ({dataUserAverageSessions}: {dataUserAverageSessions: DataUserAverageSessionsType}) => {
   
-  const CustomToolTip = ({ active, payload }) => {
+  const CustomToolTip = ({ active, payload }:{ active:boolean, payload:PayloadType }) => {
     if (active) {
       return (
         <div className="container__kpiobjective__tooltip">
@@ -27,7 +45,7 @@ const KpiObjective = ({dataUserAverageSessions}: {dataUserAverageSessions: DataU
     return null;
   }
 
-  const CustomHover = ({ points }) => {
+  const CustomHover = ({ points }:{ points:Points }) => {
     return (
       <rect
         x={points[0].x}
@@ -45,7 +63,7 @@ const KpiObjective = ({dataUserAverageSessions}: {dataUserAverageSessions: DataU
       <ResponsiveContainer width="100%" minHeight={250} >
         <LineChart data={dataUserAverageSessions} margin={{ top: 60, right: 15, left: 15, bottom: 40 }}>
           <XAxis axisLine={false} tickLine={false} dataKey="day" tick={{ fill: '#fff', fontWeight: "bold", fontSize: "12px",  dy: 30}} interval={0}/>
-          <Tooltip content={<CustomToolTip />} cursor={<CustomHover />} />
+          <Tooltip content={<CustomToolTip active={false} payload={[]} />} cursor={<CustomHover points={[]} />} />
           <Line
             dot={false}
             type="monotone"
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Logo from "../../assets/svg/logo.svg";
 import hamburger from "../../assets/svg/hamburger.svg";
 import close from "../../assets/svg/close.svg";
@@ -7,30 +7,36 @@ import "./Navbar.scss";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const updateWindowWidth = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWindowWidth);
+    return () => {
+      window.removeEventListener("resize", updateWindowWidth);
+    };
+  }, []);
 
   const handleClick = () => {
     setShowMenu(!showMenu);
-  }
+  };
 
   return (
     <header>
       <nav className="navbar">
         <img src={Logo} alt="logo SportSee" />
-        <ul>
-          <NavLink to="/" className={(nav) => (nav.isActive ? "active" : "")}>
-            <li>Accueil</li>
-          </NavLink>
-          <NavLink to="/profile">
-            <li>Profil</li>
-          </NavLink>
-          <NavLink to="/settings">
-            <li>Réglages</li>
-          </NavLink>
-          <NavLink to="/community">
-            <li>Communauté</li>
-          </NavLink>
-        </ul>
-        <ul className={showMenu ? "navbar__menu-visible" : "navbar__menu-hidden"}>
+        <ul
+          className={
+            showMenu
+              ? "navbar__menu-visible"
+              : windowWidth > 1400
+              ? "navbar__menu-visible"
+              : "navbar__menu-hidden"
+          }
+        >
           <NavLink to="/" className={(nav) => (nav.isActive ? "active" : "")}>
             <li>Accueil</li>
           </NavLink>
