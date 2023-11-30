4075f5ae73a8c35a990b1c82207493260dcd0f8a
El Ghalbzouri Adnan <elghalbzouriadnan@gmail.com>
2023-11-30 19:57:57 +0100

    FIX

--- front/src/components/sidebar/SideBar.tsx
+++ front/src/components/sidebar/SideBar.tsx
@@ -5,6 +5,12 @@ import bike from "../../assets/svg/bike.svg";
 import dumbbell from "../../assets/svg/dumbbell.svg";
 import "./SideBar.scss";
 
+type ActivitiesType = {
+  id: number;
+  icon: string;
+  alt: string;
+}[];
+
 /**
  * Composant représentant une barre latérale avec des icônes d'activités et une mention de copyright.
  * @component
@@ -12,21 +18,21 @@ import "./SideBar.scss";
  * @returns {JSX.Element} Composant SideBar
  */
 const SideBar = () => {
+  const activities: ActivitiesType = [
+    { id: 1, icon: yoga, alt: "image yoga" },
+    { id: 2, icon: swim, alt: "image swim" },
+    { id: 3, icon: bike, alt: "image bike" },
+    { id: 4, icon: dumbbell, alt: "image dumbbell" },
+  ];
+
   return (
     <aside className="sidebar">
       <ul>
-        <li>
-          <img src={yoga} alt="image yoga" />
-        </li>
-        <li>
-          <img src={swim} alt="image swim" />
-        </li>
-        <li>
-          <img src={bike} alt="image bike" />
-        </li>
-        <li>
-          <img src={dumbbell} alt="image dumbbell" />
-        </li>
+        {activities.map((activity) => (
+          <li key={activity.id}>
+            <img src={activity.icon} alt={activity.alt} />
+          </li>
+        ))}
       </ul>
       <p>Copiryght, SportSee 2020</p>
     </aside>
